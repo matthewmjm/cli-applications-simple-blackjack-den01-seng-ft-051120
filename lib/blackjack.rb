2726ab24f5c41a766c101fc1fd0dc663ae6b16a4
Matthew Malecki <mmalecki4@gmail.com>
2020-05-04 02:16:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,9 +31,7 @@ end
 def initial_round
   # code #initial_round here
   deal_card
-  card_total += deal_card
   deal_card
-  card_total += deal_card
   display_card_total(card_total)
   return card_total
 end
